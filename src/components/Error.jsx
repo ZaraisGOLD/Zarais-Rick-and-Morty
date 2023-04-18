f571935b8f9db731b3ed539c3ba5eebfbfe731e7
ZaraisGOLD <zaraisgold@gmail.com>
2023-04-18 01:05:54 -0400

    c3

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
     return (
         <div className='error'>
             <div className='error__img'>
-                <img src="./pngwing.com.png" alt="" />
+                <img src="/icon-error.png" alt="" />
             </div>
             <h2 className='error__title'>Hey! You must provide an number from 1 to 126</h2>
         </div>
