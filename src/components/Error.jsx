861396fab32387adcfeaa2973dc90998a1945790
ZaraisGOLD <zaraisgold@gmail.com>
2023-04-18 00:55:08 -0400

    correcion imagen error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,7 +4,9 @@ const Error = () => {
 
     return (
         <div className='error'>
-            <div className='error__img'></div>
+            <div className='error__img'>
+                <img src="./public/pngwing.com.png" alt="" />
+            </div>
             <h2 className='error__title'>Hey! You must provide an id from 1 to 126</h2>
         </div>
     )
